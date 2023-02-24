ed73e45dbae68555660aa13b5e718229cf802f72
JaQeQe Jackson <97812250+Teerths@users.noreply.github.com>
2023-02-24 18:38:14 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+"use strict";
+/**
+ * @type {HTMLFormElement}
+ */
+const form = document.getElementById("uv-form");
+/**
+ * @type {HTMLInputElement}
+ */
+const address = document.getElementById("uv-address");
+/**
+ * @type {HTMLInputElement}
+ */
+const searchEngine = document.getElementById("uv-search-engine");
+/**
+ * @type {HTMLParagraphElement}
+ */
+const error = document.getElementById("uv-error");
+/**
+ * @type {HTMLPreElement}
+ */
+const errorCode = document.getElementById("uv-error-code");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  try {
+    await registerSW();
+  } catch (err) {
+    error.textContent = "Failed to register service worker.";
+    errorCode.textContent = err.toString();
+    throw err;
+  }
+
+  const url = search(address.value, searchEngine.value);
+  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+});
+
+function go(value) {
+  let iframe = document.querySelector(".iframe.active");
+  window.navigator.serviceWorker
+    .register("./uv/uv.sw.js", {
+      scope: __uv$config.prefix,
+    })
+    .then(() => {
+      let url = value.trim();
+      if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
+      else if (!(url.startsWith("https://") || url.startsWith("http://")))
+        url = "https://" + url;
+      location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+ 
+    });
+}
